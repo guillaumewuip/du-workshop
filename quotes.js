46d867f9c04dae70c9849f322c7a77aab26d0624
Guillaume Wuip <guillaume@wuips.com>
2017-09-21 11:01:33 +0200

    Don't use googleapis

--- quotes.js
+++ quotes.js
@@ -1,22 +1,21 @@
 
-const
-  google = require('googleapis'),
-  sheets = google.sheets('v4');
+const csv = require('csv');
+const fs = require('fs');
 
-const RANGE = 'Quotes!B:D';
+const FILE = './citations.csv';
 
-const readSpreadsheeet = (id, range, api_key) => {
+const readSpreadsheeet = (file) => {
   return new Promise((resolve, reject) => {
-    sheets.spreadsheets.values.get({
-      auth:          api_key,
-      spreadsheetId: id,
-      range:         range,
-    }, (err, response) => {
+    fs.readFile(FILE, (err, data) => {
       if (err) {
         reject(err);
-      } else {
-        resolve(response.values);
+        return;
       }
+
+      csv.parse(data, (err, rows) => {
+        console.log(rows);
+        resolve(rows);
+      });
     });
   });
 };
@@ -25,17 +24,15 @@ const random = (max) => Math.floor(Math.random() * (max + 1));
 
 const formatQuote = (data) => {
   return {
-    content: data[0] !== '' ? data[0] : undefined,
-    author:  data[1] !== '' ? data[1] : undefined,
+    content: data[1] !== '' ? data[1] : undefined,
+    author:  data[2] !== '' ? data[2] : undefined,
   };
 };
 
 module.exports = (config) => ({
-  random: () => readSpreadsheeet(
-      config.SPREADSHEET_ID,
-      RANGE,
-      config.GOOGLE_API_KEY
-    ).then((quotes) => {
+  readSpreadsheeet: () => readSpreadsheeet(FILE),
+  random: () => readSpreadsheeet(FILE)
+    .then((quotes) => {
       const index = random(quotes.length);
       return formatQuote(quotes[index]);
     }),
